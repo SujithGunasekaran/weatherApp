28794aa72fef67b2e9b60a9c56ae789852e76cca
SujithGunasekaran <mydevelopstar@gmail.com>
2021-10-08 16:56:18 +0530

    Initial commit weather application

---
+++ weatherapp/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { FC, Fragment } from 'react';
+
+
+const App: FC = () => {
+
+  return (
+    <Fragment>
+      <div>App Component</div>
+    </Fragment>
+  )
+
+}
+
+export default App;
